7f8a1ab09f316ccdec786add6cdaffb7409af764
Donald Lieu <dclieu@ucdavis.edu>
2020-01-19 02:58:40 -0800

    i think i added a home page but it's blank

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 urlpatterns = [
     path('reports/', include('reports.urls')),
     path('admin/', admin.site.urls),
+    path('',TemplateView.as_view(template_name="home.html"))
 ]
